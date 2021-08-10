89351478f787f755569e111cf2f3c897b59e67ba
CimaraOliveira <cimarinhaoliveira@hotmail.com>
2021-08-10 17:16:33 -0300

    adicionando function longin

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, Image, View } from 'react-native';
 import { Input } from '../components/Input';
 import GlobalStyles from '../styles/GlobalStyles';
 import { MainButton } from '../components/MainButton';
-
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { emailValidacao } from '../validacao/emailvalidacao';
+import { senhaValidacao } from '../validacao/senhaValidacao';
 
 
 export function Login({ navigation }) {
+
+  const keyAsyncStorage = "@RuasLimpas:cadastros";
+  /*const [email, setEmail] = useState('');
+   const [senha, setSenha] =  useState('');
+   const [users, setUsers] = useState([]); */
+   const [email, setEmail] = useState({ value: '', error: '' })
+   const [senha, setSenha] = useState({ value: '', error: '' })
+  async function handleLogin() {
+
+    const emailError = emailValidacao(email.value);
+    const senhaError = senhaValidacao(senha.value);
+
+    if (emailError || senhaError) {
+      setEmail({ ...email, error: emailError })
+      setSenha({ ...senha, error: senhaError })
+      return
+    }
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Home' }],
+    })
+
+
+
+  }
+
+
 
   return (
 
     <View style={GlobalStyles.screenContainer}>
       <Image style={styles.imagem} source={require('../imgs/R.png')} />
 
-      <Input placeholder="E-mail" />
-      <Input placeholder="Senha" secureTextEntry={true} />
+      <Input placeholder="E-mail"
+        value={email.value}
+        onChangeText={(text) => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        textContentType="emailAddress"
+        keyboardType="email-address" />
+        
+      <Input placeholder="Senha"
+        value={senha.value}
+        onChangeText={(text) => setSenha({ value: text, error: '' })}
+        error={!!senha.error}
+        errorText={senha.error}
+        secureTextEntry
+        secureTextEntry={true} />
 
-      <MainButton title="Entrar" />
+      <MainButton title="Entrar" onPress={handleLogin} />
 
       <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Cadastro')}>
         <Text style={styles.text}>Cadastre-se</Text>
---
+++ src/validacao/emailvalidacao.js
@@ -0,0 +1,6 @@
+export function emailValidacao(email) {
+    const re = /\S+@\S+\.\S+/
+    if (!email) return "O email não pode estar vazio!"
+    if (!re.test(email)) return 'Opa! Precisamos de um endereço de e-mail válido!'
+    return ''
+  }
